189ea2aded400188f4eb50b19ca9b201170ad8fb
Mẫn <vanman.lqd@gmail.com>
2018-03-22 14:45:39 +0700

    delete unuse file and folder

--- PresentationApplication/Interfaces/IStoryClickListener.cs
+++
@@ -1,9 +0,0 @@
-﻿using Android.Views;
-
-namespace PresentationApplication.Interfaces
-{
-    public interface IStoryClickListener
-    {
-        void OnClick(View itemView, int position);
-    }
-}
